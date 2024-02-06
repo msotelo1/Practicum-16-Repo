208927f43619b32d847179d2ff8ecd3380a9f0ff
Charles Dierbach <cdierbach@towson.edu>
2024-02-06 14:30:41 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,33 +1,26 @@
-// Code for Practicum 17 (More on Operator Precedence)
-// (TO COMPLETE)
+// Code for Practicum 16 (Short-Circuit Evaluation)
+// (TO UPDATE)
 import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
-    double var1, var2, var3, var4;
-    double result;
-
-    // init
-    var1 = 10.0;
-    var2 = 2.0;
-    var3 = 5.0;
-    var4 = 20.0;
-
-    // should display the value 80
-    result = var1 * var2 / var3 * var4;
-    System.out.println("result = " + result);
-
-    // should display the value 0.2
-    result = var1 * var2 / var3 * var4;
-    System.out.println("result = " + result);
+    Scanner input = new Scanner(System.in);
     
-    // should display the value 1.0
-    result = var1 / var2 / var3;
-    System.out.println("result = " + result);
+    int numerator, denominator;
 
-    // should display the value 25.0
-    result = var1 / var2 / var3;
-    System.out.println("result = " + result);
+    System.out.println("Enter numerator <space> denominator: ");
+    numerator = input.nextInt();
+    denominator = input.nextInt();
     
+    if(denominator != 0 && numerator / denominator < 1)
+    
+      System.out.println(numerator + "/" + denominator +
+                         " is a proper fraction");
+    else
+      if(denominator != 0 && numerator / denominator >= 1)
+        System.out.println(numerator + "/" + denominator +
+                          " is an improper fraction");
+    else
+      System.out.println("** Invalid Denominator of 0**");
   }
 }
