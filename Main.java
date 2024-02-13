a6969bf7b9490ff2596ac3832b96ff0ad0f336d2
msotelo1 <158190815+msotelo1@users.noreply.github.com>
2024-02-13 02:39:23 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,25 +2,33 @@
 // (TO UPDATE)
 import java.util.Scanner;
 
-class Main {
+public class Main {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
-    
+
     int numerator, denominator;
 
     System.out.println("Enter numerator <space> denominator: ");
     numerator = input.nextInt();
     denominator = input.nextInt();
-    
-    if(denominator != 0 && numerator / denominator < 1)
-    
+
+    if (denominator !=0)
+      System.out.println("Input is valid");
+    else
+      System.out.println("** Invalid Denominator of 0 enter new denominator greater than 0");
+
+    denominator = input.nextInt();
+      
+
+    if(numerator / denominator < 1)
+
       System.out.println(numerator + "/" + denominator +
                          " is a proper fraction");
-    else
-      if(denominator != 0 && numerator / denominator >= 1)
+    else if(numerator / denominator >= 1)
         System.out.println(numerator + "/" + denominator +
                           " is an improper fraction");
-    else
-      System.out.println("** Invalid Denominator of 0**");
+    
+      
+
   }
 }
